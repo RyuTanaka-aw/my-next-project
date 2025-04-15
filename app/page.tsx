7bb3b53058fccc260c235f5a661a404737a16e4a
Ryu Tanaka <tanaka@anagram-works.jp>
2025-04-15 16:37:40 +0900

    4章まで完了

--- app/page.tsx
+++ app/page.tsx
@@ -42,8 +42,10 @@ export default function Home() {
     <>
       <section className={styles.top}>
         <div>
-          <h1 className={styles.title}>テクノロジーのちからで世界を変える</h1>
-          <p className={styles.description}>テスト</p>
+          <h1 className={styles.title}>テクノロジーの力で世界を変える</h1>
+          <p className={styles.description}>
+            私たちは市場をリードしているグローバルテックカンパニーです。
+          </p>
         </div>
         <Image
           className={styles.bgimg}
