d4265c082b9a770d3485830ea0ed0adcda3c72b0
Ryu Tanaka <tanaka@anagram-works.jp>
2025-05-16 15:20:16 +0900

    9章まで完了

---
+++ app/_components/ContactForm/index.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { createContactData } from "@/app/_actions/contact";
+import { useFormState } from "react-dom";
+import styles from "./index.module.css";
+
+const initialState = {
+  status: "",
+  message: "",
+};
+
+export default function ContactForm() {
+  const [state, formAction] = useFormState(createContactData, initialState);
+  console.log(state);
+  if (state.status === "success") {
+    return (
+      <p className={styles.success}>
+        お問い合わせいただき、ありがとうございます。
+        <br />
+        お返事まで今しばらくお待ちください。
+      </p>
+    );
+  }
+  return (
+    <form className={styles.form} action={formAction}>
+      <div className={styles.horizon}>
+        <div className={styles.item}>
+          <label htmlFor="lastname" className={styles.label}>
+            姓
+          </label>
+          <input
+            type="text"
+            id="lastname"
+            name="lastname"
+            className={styles.textfield}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="firstname" className={styles.label}>
+            名
+          </label>
+          <input
+            type="text"
+            id="firstname"
+            name="firstname"
+            className={styles.textfield}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="company" className={styles.label}>
+            会社名
+          </label>
+          <input
+            type="text"
+            id="company"
+            name="company"
+            className={styles.textfield}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="email" className={styles.label}>
+            メールアドレス
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className={styles.textfield}
+          />
+        </div>
+        <div className={styles.item}>
+          <label htmlFor="message" className={styles.label}>
+            メッセージ
+          </label>
+          <textarea
+            className={styles.textarea}
+            name="message"
+            id="message"
+          ></textarea>
+        </div>
+        <div className={styles.actions}>
+          {state.status === "error" && (
+            <p className={styles.error}>{state.message}</p>
+          )}
+          <input type="submit" value="送信する" className={styles.button} />
+        </div>
+      </div>
+    </form>
+  );
+}
